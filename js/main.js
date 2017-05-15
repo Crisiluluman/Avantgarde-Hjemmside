54aaa85d1087e0c99b3bb5d00ae328cca04ea61a
jens210 <jensvk94@gmail.com>
2017-05-15 13:28:47 +0200

    slideshow buttons

--- js/main.js
+++ js/main.js
@@ -12,3 +12,29 @@ function carousel() {
     x[slideIndex-1].style.display = "block";
     setTimeout(carousel, 3000); // Change image every 3 seconds
 }
+
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1;}
+  if (n < 1) {slideIndex = x.length;}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " w3-white";
+}
